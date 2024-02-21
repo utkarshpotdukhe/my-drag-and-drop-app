42389c4780e3e933a4c49fdf62cc34119a9b0fb1
Utkarsh Potdukhe <utkarsh16potdukhe@gmail.com>
2024-02-21 15:23:38 +0530

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import "./App.css";
+import { useState } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  Box,
+} from "@mui/material";
+
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap,
+} from "react-grid-dnd";
+
+import cars from "./api/mock.car.json";
+
+interface ICar {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+}
+
+export default function App() {
+  const [items, setItems] = useState<ICar[]>(cars);
+
+  function onChange(
+    sourceId: string,
+    sourceIndex: number,
+    targetIndex: number
+  ) {
+    const nextState = swap(items, sourceIndex, targetIndex);
+    setItems(nextState);
+  }
+
+  return (
+    <Box className="App">
+      <GridContextProvider onChange={onChange}>
+        <GridDropZone
+          id="items"
+          boxesPerRow={4}
+          rowHeight={280}
+          style={{ height: 280 * Math.ceil(items.length / 4) }}
+        >
+          {items.map((item: ICar) => (
+            <GridItem key={item.id}>
+              <Card
+                sx={{ marginRight: 2, marginBottom: 2, cursor: "-webkit-grab" }}
+              >
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={item.image}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {item.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {item.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </GridItem>
+          ))}
+        </GridDropZone>
+      </GridContextProvider>
+
+      
+    </Box>
+  );
+}
